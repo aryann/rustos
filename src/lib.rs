731c1401d4cb87416ca809c60f0dfbb563bd59e4
Aryan Naraghi <aryan.naraghi@gmail.com>
2020-04-19 12:46:53 -0700

    Changes the point of entry to the bootloader-defined function.

--- src/lib.rs
+++ src/lib.rs
@@ -43,8 +43,13 @@ pub fn halt_loop() -> ! {
 }
 
 #[cfg(test)]
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
     init();
     test_main();
     halt_loop();
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@
 #![test_runner(rustos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rustos::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     rustos::init();
 
     println!("hello");
