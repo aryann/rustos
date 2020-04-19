186469dc191f5799363966b6c28b29d1777cbe02
Aryan Naraghi <aryan.naraghi@gmail.com>
2020-04-19 11:42:46 -0700

    Adds an interrupt handler for page faults.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -36,6 +36,7 @@ lazy_static! {
             idt.double_fault
                 .set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_INTERRUPT_STACK_TABLE_IDX);
+            idt.page_fault.set_handler_fn(page_fault_handler);
         }
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
@@ -57,6 +58,24 @@ extern "x86-interrupt" fn double_fault_handler(
     _error_code: u64,
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+use crate::halt_loop;
+use x86_64::structures::idt::PageFaultErrorCode;
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: &mut InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+    use x86_64::registers::control::Cr3;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed address: {:?}", Cr2::read());
+    println!("Level 4 page table physical address: {:?}", Cr3::read());
+    println!("Error code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    halt_loop();
 }
 
 extern "x86-interrupt" fn timer_interrupt_handler(_stack_frame: &mut InterruptStackFrame) {
